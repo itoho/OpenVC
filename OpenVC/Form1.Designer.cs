b0537868b711441966fcaf3799e8105785ac8bda
BuildTools <unconfigured@null.spigotmc.org>
2020-09-12 21:42:53 +0900

    プロジェクト ファイルを追加します。

---
+++ OpenVC/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace OpenVC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(103, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(173, 101);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "キャプチャ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "cam index";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10"});
+            this.comboBox2.Location = new System.Drawing.Point(12, 24);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(55, 20);
+            this.comboBox2.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 50);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(82, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "停止";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 95);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(82, 19);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "20";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "閾値";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(288, 143);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ OpenVC/Form1.cs
@@ -0,0 +1,149 @@
+﻿using OpenCvSharp;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OpenVC
+{
+    public partial class Form1 : Form
+    {
+        public int enable = 1;
+        public int sikiiti = 20;
+        Queue<StringBuilder> queue = new Queue<StringBuilder>();
+        public Form1()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int camindex = comboBox2.SelectedIndex;
+            var capture = new VideoCapture(camindex);
+            int fps = 30;
+            int sleepTime = (int)Math.Round((decimal)1000 / fps);
+            using (var window = new Window("capture"))
+            {
+                Mat img = new Mat();
+                Mat dst = new Mat();
+                while (enable==1)
+                {
+                    capture.Read(img);
+                    //if (int.Parse(textBox1.Text) == null) textBox1.Text = "0";
+                    if (img.Empty()) break;
+                        
+                    Cv2.CvtColor(img, dst, ColorConversionCodes.BGR2GRAY);
+                    Cv2.Resize(dst,dst,new OpenCvSharp.Size(255,255));
+                    Cv2.AdaptiveThreshold(dst, dst, 255,AdaptiveThresholdTypes.MeanC,ThresholdTypes.Binary,11,int.Parse(textBox1.Text));
+                        //dst2 = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY,11,float(args[1]))
+                    
+                    int height = dst.Height;
+                    int width = dst.Width;
+                    unsafe
+                    {
+                        int kokuten = 0;
+                        StringBuilder LK= new StringBuilder("");//Line Kisuu  ����C���̕�����
+                        StringBuilder LG= new StringBuilder("");//Line Guusuu �������C���̕�����
+                        
+                        
+
+                        byte* b = dst.DataPointer;
+                        for (int i = 0; i < height; i++)
+                        {
+                            int oldx = 0;
+                            int flug = 0;
+                            for (int j = 0; j < width; j++)
+                            {
+                                //byte valueAt = b[0];
+                               // b[0] = (byte)(b[0]/2);
+                                b += 1;
+                                if (b[0] == 0)
+                                {
+                                    flug += 1;
+                                    if (flug == 1)
+                                    {
+                                        oldx = j;
+                                    }
+                                }
+                                else
+                                {
+                                    if (flug != 0)
+                                    {
+                                        kokuten++;
+                                        if (i % 2 == 0)
+                                        {
+                                            //snprintf(oneelement, sizeof(oneelement), "%d %d %d\n", oldx, x - 1, y);
+                                            //strcat(LK, oneelement);
+                                            LK.AppendFormat("{0} {1} {2}\n", oldx, j - 1, i);
+                                        }
+                                        else
+                                        {
+                                            //snprintf(oneelement, sizeof(oneelement), "%d %d %d\n", oldx, x - 1, y);
+                                            //strcat(LG, oneelement);
+                                            LG.AppendFormat("{0} {1} {2}\n", oldx, j - 1, i);
+                                        }
+                                        flug = 0;
+                                    }
+
+                                }
+
+
+                            }
+                            if (flug != 0)
+                            {    //last pixel
+                                kokuten++;
+                                if (i % 2 == 0)
+                                {
+                                    //char oneelement[64] = "";
+                                    //snprintf(oneelement, sizeof(oneelement), "%d %d %d\n", oldx, 255, y);
+                                    //strcat(LK, oneelement);
+                                    LK.AppendFormat("{0} {1} {2}\n", oldx, 255, i);
+                                }
+                                else
+                                {
+                                    //char oneelement[64] = "";
+                                    //snprintf(oneelement, sizeof(oneelement), "%d %d %d\n", oldx, 255, y);
+                                    //strcat(LG, oneelement);
+                                    LG.AppendFormat("{0} {1} {2}\n", oldx,255, i);
+                                }
+                                flug = 0;
+                            }
+                        }
+
+                        LK.Append(LG);
+                        queue.Enqueue(LK);
+                    }
+
+                
+                
+                window.ShowImage(dst);
+                Cv2.WaitKey(sleepTime);
+                }
+                Cv2.DestroyWindow("capture");
+                
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (enable == 1)
+            {
+                enable = 0;
+                this.Text = "開始";
+            }
+            else
+            {
+                enable = 1;
+                this.Text = "停止";
+            }
+        }
+    }
+}
